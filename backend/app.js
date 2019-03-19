0de32d14f59b5a60d03d13cbf6f74d921a6c92ab
Nivolves2000 <48019409+Nivolves2000@users.noreply.github.com>
2019-03-20 00:29:32 +0200

    Files upload (#2)
    * file upload 0.0
    
    * file upload 0.1
    
    * file upload 0.2

---
+++ backend/app.js
@@ -0,0 +1,88 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const walk = require('walk');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use('/data', express.static(__dirname + '/data'));
+
+function csvToJson() {
+  const results = [];
+
+  let walker = walk.walk('./data', { followLinks: false });
+
+  walker.on('file', function (root, stat, next) {
+    next();
+    let filename = `${stat.name}`;
+
+    fs.createReadStream(`./data/${filename}`)
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        let filenameJSON = `${stat.name}`.slice(0, -4);
+        const data = JSON.stringify(results);
+        fs.writeFileSync(`./json/${filenameJSON}.json`, data);
+      });
+  });
+}
+
+app.post('/upload', (req, res, next) => {
+  let file = req.files.file;
+
+  file.mv(`${__dirname}/data/${req.files.file.name}`, function (err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (req.files.file.name.substr(req.files.file.name.length - 3) !== "csv") {
+      fs.unlinkSync(`./data/${req.files.file.name}`);
+      return res.status(415).send(err);
+    }
+    if (req.files.file.name.slice(0, -4).length !== 5) {
+      fs.unlinkSync(`./data/${req.files.file.name}`);
+      return res.status(406).send(err);
+    }
+    res.json({ file: `data/${req.files.file.name}` });
+
+    csvToJson();
+  });
+
+})
+
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(8000, () => {
+  console.log('8000');
+});
+
+module.exports = app;
+
+
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from "./components/Form";
+
+const App = () => (
+  <div>
+    <h1>File Upload</h1>
+    <Form />
+  </div>
+);
+
+export default App;
